b18b1ba88e5cef9ecd925b25c184622e9ac39468
Mohamad Rajabifard <mo.rajbi@gmail.com>
2022-05-08 16:37:09 +0430

    Fix mobile view

--- app/routes/docs.tsx
+++ app/routes/docs.tsx
@@ -4,9 +4,12 @@ import {
   Outlet,
   useLoaderData,
   useLocation,
+  useTransition,
 } from "remix";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { getNavigationLinks, MiniPage } from "~/utils/pageCache.server";
+import { SpinnerCircular } from "spinners-react";
+import { MenuIcon } from "~/components/MenuIcon";
 
 export const rootPageId = "Overview-e65926fae6094b1a962bf9ea44489139";
 
@@ -20,14 +23,27 @@ export const loader: LoaderFunction = async ({ request }) => {
 
 export default function Dashboard() {
   const { pages } = useLoaderData<{ pages: MiniPage[] }>();
+  let { state } = useTransition();
+  let [menuOpen, setMenuOpen] = useState(false);
 
+  let menuOpenStyles = menuOpen
+    ? `h-screen w-screen fixed top-0 left-0 z-10`
+    : "h-20";
+
+  useEffect(() => {
+    if (state === "loading") {
+      setMenuOpen(false);
+    }
+  }, [state]);
   return (
     <div
       style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}
       className="mx-auto"
     >
-      <div className="flex flex-row max-w-screen-2xl mx-auto">
-        <aside className="w-72 bg-sidebg px-7 pt-7 flex-shrink-0">
+      <div className="flex flex-col sm:flex-row h-screen max-w-screen-2xl mx-auto">
+        <aside
+          className={`sm:w-72 sm:h-full w-full bg-sidebg px-7 pt-7 flex-shrink-0 ${menuOpenStyles} `}
+        >
           <div className="flex flex-row items-center">
             <img src="/docs-assets/images/logo.svg" alt="logo" width={30} />
             <h1 className="text-xl ml-1">
@@ -36,9 +52,18 @@ export default function Dashboard() {
                 <span className="ml-1 text-textblue font-pacifico">Docs</span>
               </Link>
             </h1>
+
+            <div
+              className="ml-auto w-10 h-10 rounded-lg hover:bg-slate-100 items-center justify-center flex sm:hidden"
+              onClick={() => {
+                setMenuOpen((open) => !open);
+              }}
+            >
+              <MenuIcon />
+            </div>
           </div>
 
-          <div className="pt-5">
+          <div className="pt-7">
             {pages.map((page) => (
               <SideLink
                 key={page.id}
@@ -49,18 +74,34 @@ export default function Dashboard() {
             ))}
           </div>
         </aside>
-        <main className="w-full h-full">
-          <Outlet />
+        <main className="w-full h-full flex-grow">
+          {state === "loading" ? (
+            <div className="flex items-center justify-start w-full h-full">
+              <SpinnerCircular
+                color="#51b9ff"
+                secondaryColor="#c3e5f9"
+                size={50}
+                thickness={60}
+                style={{ margin: "0 auto" }}
+              />
+            </div>
+          ) : (
+            <Outlet />
+          )}
         </main>
       </div>
     </div>
   );
 }
 
-const selectedColor = "#DFECFF";
+const selectedColor = "#e5effe";
 
 const SideLinkText = ({ children }: any) => {
-  return <p className={`text-sm select-none text-slate-700`}>{children}</p>;
+  return (
+    <p className={`text-sm select-none text-slate-700 active:text-slate-500`}>
+      {children}
+    </p>
+  );
 };
 
 //@ts-ignore
@@ -79,46 +120,48 @@ const SideLink = ({
   let hasChildren = items && items.length > 0;
   let inRootPage = pathname === pageLink;
 
-  let titleNode = <SideLinkText>{title}</SideLinkText>;
+  let titleNode = (
+    <div
+      onClick={() => {
+        setOpen(!open);
+      }}
+      style={{
+        backgroundColor: inRootPage ? selectedColor : undefined,
+        borderRadius: 8,
+      }}
+      className="flex flex-row items-center h-7 mb-0.5 hover:bg-slate-100 transition-colors duration-100 cursor-pointer"
+    >
+      {hasChildren ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="19"
+          height="19"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="#A2A2A2"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          transform={open ? "rotate(90)" : ""}
+          style={{ paddingTop: 1 }}
+        >
+          <polyline points="9 18 15 12 9 6"></polyline>
+        </svg>
+      ) : (
+        <div style={{ width: 19 }} />
+      )}
+
+      <SideLinkText>{title}</SideLinkText>
+    </div>
+  );
 
   return (
     <>
-      <div
-        onClick={() => {
-          setOpen(!open);
-        }}
-        style={{
-          backgroundColor: inRootPage ? selectedColor : undefined,
-          borderRadius: 8,
-        }}
-        className="flex flex-row items-center h-7"
-      >
-        {hasChildren ? (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="19"
-            height="19"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="#A2A2A2"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            transform={open ? "rotate(90)" : ""}
-            style={{ paddingTop: 3 }}
-          >
-            <polyline points="9 18 15 12 9 6"></polyline>
-          </svg>
-        ) : (
-          <div style={{ width: 19 }} />
-        )}
-
-        {pageLink && !hasChildren ? (
-          <Link to={pageLink}>{titleNode}</Link>
-        ) : (
-          titleNode
-        )}
-      </div>
+      {pageLink && !hasChildren ? (
+        <Link to={pageLink}>{titleNode}</Link>
+      ) : (
+        titleNode
+      )}
 
       {open &&
         hasChildren &&
